ca8c0b98d07df05cc42e255b1443f7ffac12516a
Durga <durgaathapa25@gmail.com>
2025-12-22 12:24:17 +0545

    setup routing

---
+++ models/bookModels.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const bookSchema= new mongoose.Schema({
+    title :{
+        type:String,
+        unique:true,
+        required:true
+    },
+description:{
+    type:String,
+    required:true
+},
+author:{
+    type:String,
+    required:true
+},
+price:Number
+},
+{timestamps:true}
+)
